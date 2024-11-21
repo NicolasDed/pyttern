57615404374ab974724f5673ad9fbcaa8f9dca93
NicolasDed <nicolas-dedoyard@outlook.com>
2024-11-21 17:59:48 +0100

    REFACTOR main

--- pyttern/main.py
+++ pyttern/main.py
@@ -1,103 +1,60 @@
 import argparse
-import glob
-import io
-from functools import cache
+# import glob
+# from functools import cache
+# from tqdm import tqdm
 
-from antlr4 import FileStream, CommonTokenStream, InputStream
-from loguru import logger
-from tqdm import tqdm
+from .language_processors import get_processor
 
-from .PytternListener import ConsolePytternListener
-from .antlr import Python3Parser
-from .antlr.Python3Lexer import Python3Lexer
-from .pyttern_error_listener import Python3ErrorListener
-from .pytternfsm.python_visitor import Python_Visitor
-from .pytternfsm.tree_pruner import TreePruner
-from .simulator.simulator import Simulator
+def run_application():
+    from .visualizer.web import application
+    application.app.run(debug=True)
+    
 
+# def match_files(pattern_path, code_path, strict_match=True, match_details=False):
+#     try:
+#         pattern = generate_tree_from_file(pattern_path)
+#         code = generate_tree_from_file(code_path)
+#     except Exception as e:
+#         assert False, e
 
-def match_files(pattern_path, code_path, strict_match=True, match_details=False):
-    try:
-        pattern = generate_tree_from_file(pattern_path)
-        code = generate_tree_from_file(code_path)
-    except Exception as e:
-        assert False, e
+#     fsm = Python_Visitor(strict_match).visit(pattern)
 
-    fsm = Python_Visitor(strict_match).visit(pattern)
-
-    simu = Simulator(fsm, code)
-    simu.start()
-    while len(simu.states) > 0:
-        simu.step()
-    if match_details:
-        return len(simu.match_set.matches) > 0, simu.match_set.matches
-    return len(simu.match_set.matches) > 0
+#     simu = Simulator(fsm, code)
+#     simu.start()
+#     while len(simu.states) > 0:
+#         simu.step()
+#     if match_details:
+#         return len(simu.match_set.matches) > 0, simu.match_set.matches
+#     return len(simu.match_set.matches) > 0
 
 
-def match_wildcards(pattern_path, code_path, strict_match=True, match_details=False):
-    """
-    Match all python files with all pattern files.
-    The path_pattern_with_wildcards and path_python_with_wildcard
-    can contain wildcards. The function returns a dictionary with the results of the matches.
-    :param pattern_path: Path to the pattern files with wildcards.
-    :param code_path: Path to the python files with wildcards.
-    :param strict_match: If True, the match will be strict, otherwise it will be soft.
-    :param match_details: If True, the function will return the match details.
-    :return: Dictionary with the results of the matches.
-    """
-    ret = {}
-    patterns_filespath = glob.glob(str(pattern_path))
-    pythons_filespath = glob.glob(str(code_path))
+# def match_wildcards(pattern_path, code_path, strict_match=True, match_details=False):
+#     """
+#     Match all python files with all pattern files.
+#     The path_pattern_with_wildcards and path_python_with_wildcard
+#     can contain wildcards. The function returns a dictionary with the results of the matches.
+#     :param pattern_path: Path to the pattern files with wildcards.
+#     :param code_path: Path to the python files with wildcards.
+#     :param strict_match: If True, the match will be strict, otherwise it will be soft.
+#     :param match_details: If True, the function will return the match details.
+#     :return: Dictionary with the results of the matches.
+#     """
+#     ret = {}
+#     patterns_filespath = glob.glob(str(pattern_path))
+#     pythons_filespath = glob.glob(str(code_path))
 
-    try:
-        pythons_filespath = tqdm(pythons_filespath)
-    except NameError:
-        pass
+#     try:
+#         pythons_filespath = tqdm(pythons_filespath)
+#     except NameError:
+#         pass
 
-    for python_filepath in pythons_filespath:
-        for pattern_filepath in patterns_filespath:
-            result = match_files(pattern_filepath, python_filepath, strict_match, match_details)
-            if python_filepath not in ret:
-                ret[python_filepath] = {}
-            ret[python_filepath][pattern_filepath] = result
-    return ret
-
-
-@cache
-def generate_tree_from_code(code):
-    code = code.strip()
-    stream = InputStream(code)
-    return generate_tree_from_stream(stream)
-
-
-@cache
-def generate_tree_from_stream(stream):
-    logger.info("Generating tree")
-    lexer = Python3Lexer(stream)
-    stream = CommonTokenStream(lexer)
-    py_parser = Python3Parser(stream)
-
-    error = io.StringIO()
-
-    py_parser.removeErrorListeners()
-    error_listener = Python3ErrorListener(error)
-    py_parser.addErrorListener(error_listener)
-
-    tree = py_parser.file_input()
-    if len(error_listener.symbol) > 0:
-        raise IOError(
-            f"Syntax error in {stream} at line {error_listener.line} "
-            f"({repr(error_listener.symbol)}) : {error.getvalue()}")
-
-    pruned_tree = TreePruner().visit(tree)
-
-    return pruned_tree
-
-
-@cache
-def generate_tree_from_file(file):
-    file_input = FileStream(file, encoding="utf-8")
-    return generate_tree_from_stream(file_input)
+#     for python_filepath in pythons_filespath:
+#         for pattern_filepath in patterns_filespath:
+#             result = match_files(pattern_filepath, python_filepath, strict_match, match_details)
+#             if python_filepath not in ret:
+#                 ret[python_filepath] = {}
+#             ret[python_filepath][pattern_filepath] = result
+#     return ret
 
 
 def run_application():
@@ -109,6 +66,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--web", action="store_true")
+    parser.add_argument("--lang", choices=['python', 'java'], required=True, help="Specify the language (python/java)")
 
     parser.add_argument("pattern")
     parser.add_argument("code")
@@ -119,6 +77,7 @@ def main():
         run_application()
         return
 
+    processor = get_processor(args.lang) # Get processor behaviour adapted to language
     pattern = args.pattern
     code = args.code
 
